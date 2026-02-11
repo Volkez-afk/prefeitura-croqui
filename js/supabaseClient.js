a6a8851554a0b3a1a6d2ed648ac5594d02b4b8b9
Volkez-afk <viniciusmegetto@gmail.com>
2026-02-11 13:13:47 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const SUPABASE_URL = 'https://wtgxzswahhwqjuasnrie.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_uMEfaVJdfXEt264vEZVrFA_GoJDC1gk'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0Z3h6c3dhaGh3cWp1YXNucmllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3ODI4OTIsImV4cCI6MjA4NjM1ODg5Mn0.E-_oZ3o1517rY5w0e0NMaHiu4L0vLRo6v4-5EwkS3Uk'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
